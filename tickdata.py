bfda2da5cd9d0ac371cc6644da73fcd0183b9a97
Pangjing-Wu <pangjing.wu@outlook.com>
2020-06-11 12:48:34 +0800

    Simplify loading data interface, fix some bugs.

--- tickdata.py
+++ tickdata.py
@@ -1,10 +1,13 @@
+import json
 import os
-import time
 import subprocess
-import psycopg2
+import time
+
 import pandas as pd
+import psycopg2
 
 # TODO complete logger.
+
 
 class H2Connection(object):
 
@@ -49,7 +52,7 @@ class H2Connection(object):
         return data
 
     def _is_h2_online(self):
-        output = subprocess.getoutput('ps -l|grep h2|grep -v grep')
+        output = subprocess.getoutput('ps -la|grep h2|grep -v grep')
         status = True if len(output) else False
         return status
 
@@ -89,60 +92,108 @@ class TickData(object):
 
     def __init__(self, data: pd.DataFrame):
         self._data = data
-        self._quote = self._data[self._data['type'] == 'quote'].dropna(axis=1).reset_indix()
-        self._trade = self._data[self._data['type'] == 'trade'].dropna(axis=1).reset_indix()
+        self._quote = self._data[self._data['type'] == 'quote']
+        self._quote = self._quote.drop('type', axis=1).dropna(axis=1).reset_index(drop=True)
+        self._trade = self._data[self._data['type'] == 'trade']
+        self._trade = self._trade.drop('type', axis=1).dropna(axis=1).reset_index(drop=True)
 
     def __len__(self):
         return self._data.shape[0]
-
-    def __str__(self):
-        return self._data
 
     @property
     def data(self):
         return self._data
 
+    @property
+    def quote_timeseries(self):
+        return self._quote['time'].values.tolist()
+
+    @property
+    def trade_timeseries(self):
+        return self._trade['time'].values.tolist()
+
     def get_quote(self, t:None or int or list = None)->pd.DataFrame:
-        quote = self._quote if t is None else self._quote.loc[t, :]
-        quote = quote.drop(['index', 'type'])
+        if t == None:
+            quote = self._quote
+        elif type(t) == int:
+            quote = self._quote[self._quote['time'] == t]
+        else:
+            quote = self._quote[self._quote['time'].isin(t)]
         return quote
 
     def get_trade(self, t:None or int or list = None)->pd.DataFrame:
-        trade = self._trade if t is None else self._trade.loc[t, :]
-        trade = trade.drop(['index', 'type'])
+        if t == None:
+            trade = self._trade
+        elif type(t) == int:
+            trade = self._trade[self._trade['time'] == t]
+        else:
+            trade = self._trade[self._trade['time'].isin(t)]
         return trade
 
-    def pre_quote(self, t:int or pd.Series)->pd.Series:
+    def pre_quote(self, t:int or pd.DataFrame)->pd.DataFrame:
         if type(t) == int:
             pass
         elif type(t) == pd.DataFrame:
             t = t['time'].iloc[0]
-        elif type(t) == pd.Series:
-            t = t['time']
         else:
-            raise TypeError("argument 't' munst be 'int', 'pd.Series', or 'pd.DataFrame'.")
+            raise TypeError("argument 't' munst be 'int' or 'pd.DataFrame'.")
         quote = self._quote[self._quote['time'] < t]
-        return None if quote.empty else quote.iloc[-1]
+        return None if quote.empty else quote.iloc[-1:]
 
-    def next_quote(self, t:int or pd.Series)->pd.Series:
+    def next_quote(self, t:int or pd.DataFrame)->pd.DataFrame:
         if type(t) == int:
             pass
         elif type(t) == pd.DataFrame:
             t = t['time'].iloc[0]
-        elif type(t) == pd.Series:
-            t = t['time']
         else:
-            raise TypeError("argument 't' munst be 'int', 'pd.Series', or 'pd.DataFrame'.")
+            raise TypeError("argument 't' munst be int or pd.DataFrame.")
         quote = self._quote[self._quote['time'] > t]
-        return None if quote.empty else quote.iloc[0]
+        return None if quote.empty else quote.iloc[0:1]
     
-    def get_trade_between(self, pre_quote:pd.Series, post_quote:None or pd.Series = None)->pd.DataFrame:
+    def get_trade_between(self, pre_quote:int or pd.DataFrame,
+                          post_quote:None or int or pd.DataFrame = None)->pd.DataFrame:
+        if type(pre_quote) == int:
+            pass
+        elif type(pre_quote) == pd.DataFrame:
+            pre_quote = pre_quote['time'].iloc[0]
+        else:
+            raise TypeError("pre_quote must be int, or pd.DataFrame")
         # use next quote if post_quote is not specified.
         if post_quote == None:
-            post_quote = self.next_quote(pre_quote)
-        time1, time2 = pre_quote['time'], post_quote['time']
-        trade = self._trade[(self._trade['time'] > time1) & (self._trade['time'] < time2)]
+            post_quote = self.next_quote(pre_quote)['time'].iloc[0]
+            if post_quote == None:
+                raise KeyError('There is no quote data after pre_quote.')
+        elif type(post_quote) == int:
+            pass
+        elif type(pre_quote) == pd.DataFrame:
+            post_quote = post_quote['time'].iloc[0]
+        else:
+            raise TypeError("post_quote must be 'None', int, or pd.Series")
+        trade = self._trade[(self._trade['time'] > pre_quote) & (self._trade['time'] < post_quote)]
         return None if trade.empty else trade
 
-    def trade_statistic(self, trade:pd.DataFrame)->pd.DataFrame:
-        return trade.groupby('price').sum()
+    def trade_sum(self, trade:pd.DataFrame)->pd.DataFrame:
+        if trade is None:
+            return None
+        elif trade.empty:
+            return None
+        else:
+            return trade[['price', 'size']].groupby('price').sum().reset_index()
+
+
+def dataset(stock, dbdir, user, psw)->TickData:
+    config = json.load(open('config/data.json', 'r'))
+    h2db = H2Connection(dbdir, user, psw, **config['h2setting'])
+    QUOTE_COLS = config['sql']['QUOTE_COLS']
+    TRADE_COLS = config['sql']['TRADE_COLS']
+    TIMES = config['sql']['TIMES']
+    if h2db.status:
+        sql = config['sql']['str'] % eval(config['sql']['pattern'])
+        data = h2db.query(sql)
+        data.columns = config['tickdata']['TICK_COLS']
+        data['type'] = None
+        for i in data.index:
+            data.loc[i, 'type'] = 'trade' if data.loc[i, 'bid1'] == None else 'quote'
+    else:
+        raise ConnectionError("cannot connect to H2 service, please strat H2 service first.")
+    return TickData(data)
