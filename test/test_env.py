3d9ac492b78f84f7a5bba8431c0b6c635e006784
Pangjing-Wu <pangjing.wu@outlook.com>
2020-07-03 21:08:38 +0800

    test_env prewriting

---
+++ test/test_env.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('./')
+
+import pandas as pd
+
+from data.tickdata import TickData
+from env import AlgorithmTrader
+
+quote = pd.read_csv('test/data/000001-quote-20140704.csv')
+trade = pd.read_csv('test/data/000001-trade-20140704.csv')
+
+td = TickData(quote, trade)
+trader = AlgorithmTrader(td, 10000, reward_function='vwap', max_level=10)
+s0 = trader.reset()
+a = (1, 0, 100)
+trader.step(a)
+trader.step(a)
+next_s, r, signal, info = trader.step(a)
+print(next_s)
+print(r)
+print(signal)
+print(info)
