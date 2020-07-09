ecb8ba5e925aa04c8e1d22dd0599f9b2d836afc9
Pangjing-Wu <pangjing.wu@outlook.com>
2020-07-09 18:28:20 +0800

    improve automatic test

---
+++ test/utils/dataloader.py
@@ -0,0 +1,20 @@
+import json
+
+import pandas as pd
+
+def load_tickdata(stock, time):
+    quote = pd.read_csv('test/data/tickdata/%s-quote-%s.csv' % (stock, time))
+    trade = pd.read_csv('test/data/tickdata/%s-trade-%s.csv' % (stock, time))
+    return quote, trade
+
+def load_case(casename):
+    filedir = 'test/data/case/' + casename
+    with open(filedir, 'r') as f:
+        cases = f.readlines()
+    cases    = [json.loads(c) for c in cases]
+    case     = [c['case'] for c in cases]
+    params   = [c['params'] for c in cases]
+    excepted = [c['excepted'] for c in cases]
+    for p, e in zip(params, excepted):
+        p['excepted'] = e
+    return case, params
