3382ceec48070a822b1e30a854c117913bb53af2
Pangjing-Wu <pangjing.wu@outlook.com>
2020-07-10 17:46:51 +0800

    reset test case structure

--- test/utils/dataloader.py
+++ test/utils/dataloader.py
@@ -2,6 +2,7 @@ import json
 
 import pandas as pd
 
+
 def load_tickdata(stock, time):
     quote = pd.read_csv('test/data/tickdata/%s-quote-%s.csv' % (stock, time))
     trade = pd.read_csv('test/data/tickdata/%s-trade-%s.csv' % (stock, time))
@@ -11,10 +12,7 @@ def load_case(casename):
     filedir = 'test/data/case/' + casename
     with open(filedir, 'r') as f:
         cases = f.readlines()
-    cases    = [json.loads(c) for c in cases]
-    case     = [c['case'] for c in cases]
-    params   = [c['params'] for c in cases]
-    excepted = [c['excepted'] for c in cases]
-    for p, e in zip(params, excepted):
-        p['excepted'] = e
+    cases  = [json.loads(c) for c in cases]
+    case   = [c['case'] for c in cases]
+    params = [[c['params'], c['excepted']] for c in cases]
     return case, params
