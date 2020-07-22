47ea28bf8b2185e3c4ed67f642f2d671e8d772e3
Pangjing-Wu <pangjing.wu@outlook.com>
2020-07-22 20:50:42 +0800

    add new manual test

---
+++ test/manual/manual_test_stastic.py
@@ -0,0 +1,24 @@
+import datetime
+import sys
+import traceback
+sys.path.append('./')
+sys.path.append('./test')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from utils.dataloader import load_tickdata, load_case
+from src.datasource.datatype import TickData
+from src.utils.statastic import *
+
+quote, trade = load_tickdata(stock='000001', time='20140704')
+data = TickData(quote, trade)
+trade = data.get_trade()
+time = [34200000, 41400000, 46800000, 54000000]
+volume_per_30min = group_trade_volume_by_time(trade, time, 1800000)
+print(volume_per_30min)
+'''
+volume_total = sum(volume_per_5sec['volume'])
+ratio = [v / volume_total for v in volume_per_5sec['volume']]
+plt.plot(list(range(len(ratio))), ratio)
+plt.show()
+'''
---
+++ test/manual/manual_test_vwap.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('./')
+sys.path.append('./test')
+
+from utils.dataloader import load_tickdata, load_case
+from src.datasource.datatype import TickData
+from src.exchange.stock import GeneralExchange
+from src.strategies.classic.vwap import VWAPAgent, VWAPEnv
+
+quote, trade = load_tickdata(stock='000001', time='20140704')
+data = TickData(quote, trade)
+trade = data.get_trade()
+time = [34200000, 41400000, 46800000, 53700000]
+
+exchange = GeneralExchange(data, 3)
+
+env = VWAPEnv(data, 1000, time, 1800000, trade, exchange.transaction_engine)
+
+agent = VWAPAgent(side='sell', threshold=0.1)
+
+s = env.reset()
+final = env.is_final()
+
+with open('test/results/manual_test_vwap.txt', 'w') as f:
+    while not final:
+        f.write('schedual ratio: %.3f\n' % s)
+        a = agent.action(s)
+        s, final, info = env.step(a)
+        f.write(info)
+
+    f.write('tasks:\n%s\n' % env.subtasks)
+    f.write('vwap: %.3f\n' % env.vwap)
