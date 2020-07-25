3ab369e96cd91512dc3e3f301ea3b3303a801492
Pangjing-Wu <pangjing.wu@outlook.com>
2020-07-25 15:47:05 +0800

    update test

--- test/manual/manual_test_vwap.py
+++ test/manual/manual_test_vwap.py
@@ -5,7 +5,7 @@ sys.path.append('./test')
 from utils.dataloader import load_tickdata, load_case
 from src.datasource.datatype import TickData
 from src.exchange.stock import GeneralExchange
-from src.strategies.classic.vwap import VWAPAgent, VWAPEnv
+from src.strategies.classic.vwap import BasicAgent, VWAPEnv
 
 quote, trade = load_tickdata(stock='000001', time='20140704')
 data = TickData(quote, trade)
@@ -16,7 +16,7 @@ exchange = GeneralExchange(data, 3)
 
 env = VWAPEnv(data, 2000, time, 1200000, trade, exchange.transaction_engine)
 
-agent = VWAPAgent(side='sell', threshold=0.1)
+agent = BasicAgent(side='sell', threshold=0.1)
 
 s = env.reset()
 final = env.is_final()
@@ -28,6 +28,6 @@ with open('test/results/manual_test_vwap.txt', 'w') as f:
         a = agent.action(s)
         s, final, info = env.step(a)
         f.write(info)
-
+        
     f.write('tasks:\n%s\n' % env.subtasks)
     f.write('vwap: %.3f\n' % env.vwap)
