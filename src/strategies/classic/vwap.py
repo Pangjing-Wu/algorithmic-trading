936528d07af4de1c837ed553e258f0ea99356da6
Pangjing-Wu <pangjing.wu@outlook.com>
2020-07-25 15:46:59 +0800

    add market vwap index

--- src/strategies/classic/vwap.py
+++ src/strategies/classic/vwap.py
@@ -14,7 +14,7 @@ argmax = lambda a: [i for i, val in enumerate(a) if (val == max(a))][0]
 
 class VWAPEnv(object):
 
-    def __init__(self, tickdata:pd.DataFrame, goal:int, time:list, interval:int,
+    def __init__(self, tickdata, goal:int, time:list, interval:int,
                  hist_trade:pd.DataFrame, transaction_engine):
 
         if len(time) < 2 and len(time) % 2 != 0:
@@ -36,9 +36,9 @@ class VWAPEnv(object):
             )
         self._subtasks = pd.DataFrame(subtasks)
 
-        self._time = []
+        self._time_list = []
         for i in range(0, len(time), 2):
-            self._time += [t for t in self._data.quote_timeseries if t >= time[i] and t < time[i+1]]
+            self._time_list += [t for t in self._data.quote_timeseries if t >= time[i] and t < time[i+1]]
 
         self._level_space = ['bid1', 'ask1']
         self._level_space_n = len(self._level_space)
@@ -70,13 +70,22 @@ class VWAPEnv(object):
         vwap = vwap / sum(size) if sum(size) != 0 else 0
         return vwap
 
+    @property
+    def market_vwap(self):
+        trade = self._data.get_trade_between(self._time_list[0], self._t)
+        price = trade['price']
+        size  = trade['size']
+        vwap  = sum(map(lambda x, y: x * y, price, size))
+        vwap = vwap / sum(size) if sum(size) != 0 else 0
+        return vwap
+
     def is_final(self):
         return self._final
 
     def reset(self):
         self._init = True
         self._final = False
-        self._t = iter(self._time)
+        self._iter = iter(self._time_list)
         self._subtasks['filled'] = 0
         self._filled = {'time': [], 'price':[], 'size':[]}
         self._order = {'time': 0, 'side': 0, 'price': 0, 'size': 0, 'pos': -1}
@@ -103,15 +112,17 @@ class VWAPEnv(object):
         if self._final == True:
             raise EnvTerminatedError
 
-        t = next(self._t)
-        info = 'At time %s, ' % t
+        self._t = next(self._iter)
+        info = 'At time %s, ' % self._t
 
-        task = self._get_subtask(t)
+        time_range = (self._subtasks['start'] <= self._t) & (self._subtasks['end'] > self._t)
+        index = self._subtasks[time_range].index[0]
+        task  = self._subtasks.loc[index]
 
         if action[2] > 0:
-            self._order = self._create_new_order(action, t)
+            self._order = self._create_new_order(action)
         else:
-            self._order['time'] = t
+            self._order['time'] = self._t
         info += 'issued order %s to exchange, ' % self._order
 
         self._order, filled = self._engine(self._order)
@@ -121,9 +132,9 @@ class VWAPEnv(object):
         for key in self._filled:
             self._filled[key] += filled[key]
         
-        schedule_ratio = self._schedule_ratio(task, t)
+        schedule_ratio = self._schedule_ratio(task)
 
-        if t == self._time[-1]:
+        if self._t == self._time_list[-1]:
             self._final = True
 
         if self._subtasks['filled'].sum() == self._subtasks['goal'].sum():
@@ -135,7 +146,7 @@ class VWAPEnv(object):
 
         return (schedule_ratio, self._final, info)
 
-    def _create_new_order(self, action, time):
+    def _create_new_order(self, action):
         '''
         Argument:
         ---------
@@ -146,22 +157,23 @@ class VWAPEnv(object):
         order: dict, keys are formed by
                ('side', 'price', 'size', 'pos').
         '''
+        time  = self._t
         side  = 'buy' if action[0] == 0 else 'sell'
         level = self._level_space[action[1]]
         price = self._data.get_quote(time)[level].iloc[0]
         order = dict(time=time, side=side, price=price, size=action[2], pos=-1)
         return order
 
-    def _get_subtask(self, time: int):
-        condition = (self._subtasks['start'] <= time) & (self._subtasks['end'] > time)
+    def _get_subtask(self):
+        condition = (self._subtasks['start'] <= self._t) & (self._subtasks['end'] > self._t)
         index = self._subtasks[condition].index[0]
         return self._subtasks.loc[index]
         
-    def _schedule_ratio(self, task, current_t):
+    def _schedule_ratio(self, task):
         if task['goal'] == 0:
             return Inf
         filled_ratio = task['filled'] / task['goal']
-        time_ratio = (current_t - task['start']) / (task['end'] - task['start'])
+        time_ratio = (self._t - task['start']) / (task['end'] - task['start'])
         if filled_ratio == 1:
             return Inf
         if time_ratio == 0:
@@ -176,14 +188,14 @@ class VWAPEnv(object):
             self._tqdm_id.append(task.name)
             self._tqdm = tqdm(desc='task %s' % task.name, total=task['goal'])
 
-        self._tqdm.set_postfix(vwap=self.vwap)
+        self._tqdm.set_postfix(vwap=self.vwap, market_vwap=self.market_vwap)
         self._tqdm.update(increment)
 
         if self._final == True:
             self._tqdm.close()
         
 
-class VWAPAgent(object):
+class BasicAgent(object):
     
     def __init__(self, side: str, threshold: int=0.1):
         self._side = 0 if side == 'buy' else 1
