eee2419c1998577914a1b3c28c0641192252f868
Pangjing-Wu <pangjing.wu@outlook.com>
2020-08-06 08:31:49 -0400

    improve class structure

--- strategies/vwap/env.py
+++ strategies/vwap/env.py
@@ -1,3 +1,4 @@
+import abc
 import sys
 sys.path.append('./')
 
@@ -8,9 +9,9 @@ from utils.errors import *
 
 
 argmax = lambda a: [i for i, val in enumerate(a) if (val == max(a))][0]
+dotmut = lambda x, y: sum([a * b for a, b in zip(x, y)])
 
-
-class TrancheEnv(object):
+class BasicTranche(object):
 
     def __init__(self, tickdata, task:pd.Series, transaction_engine:callable, level_space:list, verbose=0):
         self._data = tickdata
@@ -25,6 +26,11 @@ class TrancheEnv(object):
     def __len__(self):
         return len(self._time)
     
+    @abc.abstractproperty
+    @property
+    def observation_space_n(self):
+        pass
+
     @property
     def action_space(self):
         return list(range(len(self._level_space) + 1))
@@ -38,10 +44,6 @@ class TrancheEnv(object):
         return self._t
 
     @property
-    def observation_space_n(self):
-        return 5
-
-    @property
     def volume_profile(self):
         return self._volume_profile
 
@@ -49,7 +51,7 @@ class TrancheEnv(object):
     def vwap(self):
         price = self._filled['price']
         size  = self._filled['size']
-        vwap  = sum(map(lambda x, y: x * y, price, size))
+        vwap  = dotmut(price, size)
         vwap  = vwap / sum(size) if sum(size) != 0 else 0
         return vwap
 
@@ -58,7 +60,7 @@ class TrancheEnv(object):
         trade = self._data.get_trade_between(self._time[0], self._t)
         price = trade['price']
         size  = trade['size']
-        vwap  = sum(map(lambda x, y: x * y, price, size))
+        vwap  = dotmut(price, size)
         vwap  = vwap / sum(size) if sum(size) != 0 else 0
         return vwap
 
@@ -71,6 +73,13 @@ class TrancheEnv(object):
     def is_final(self):
         return self._final
 
+    def metrics(self):
+        ret = dict(
+            vwap=round(self.vwap, 5),
+            market_vwap=round(self.market_vwap, 5)
+            )
+        return ret
+        
     def reset(self):
         self._init = True
         self._final = False
@@ -78,30 +87,142 @@ class TrancheEnv(object):
         self._t = next(self._iter)
         self._filled = {'time': [], 'price':[], 'size':[]}
         self._order = {'time': 0, 'side': 'buy', 'price': 0, 'size': 0, 'pos': -1}
-        state = [self._t / 1000, self._task['start'] / 1000, self._task['end'] / 1000, self._task['goal'], sum(self._filled['size'])]
         if self._verbose > 0:
             self._tqdm = tqdm(desc='task %s' % self._task.name, total=self._task['goal'])
+
+    @abc.abstractmethod
+    def step(self):
+        pass
+
+    def _action2order(self, action):
+        '''
+        Argument:
+        ---------
+        action: list, tuple, or array likes [side, level, size], where side: int, 0=buy, 1=sell.
+        
+        Return:
+        -------
+        order: dict, keys are ('side', 'price', 'size', 'pos').
+        '''
+        time  = self._t
+        side  = 'buy' if action[0] == 0 else 'sell'
+        level = self._level_space[action[1]]
+        price = self._data.get_quote(time)[level].iloc[0]
+        order = dict(time=time, side=side, price=price, size=action[2], pos=-1)
+        return order
+
+
+class HardConstrainTranche(BasicTranche):
+    
+    def __init__(self, tickdata, task:pd.Series, transaction_engine:callable, level_space:list, verbose=0):
+        super().__init__(tickdata, task, transaction_engine, level_space, verbose)
+
+    @property
+    def observation_space_n(self):
+        return 7
+        
+    def reset(self):
+        super().reset()
+        prices = self._data.quote_board(self._t).loc[self._level_space, 'price'].values.tolist()
+        state  = [
+            self._t / 1000,
+            self._task['start'] / 1000, self._task['end'] / 1000,
+            self._task['goal'], sum(self._filled['size']),
+            *prices
+            ]
         return state
 
     def step(self, action):
         '''
         Argument:
         ---------
-        action: list, tuple, or array like,
-            forms as [side, level, size], where side in {0=buy, 1=sell}.
+        action: list, tuple, or array likes [side, level, size], where side in {0=buy, 1=sell}.
+        
         Returns:
         --------
-        schedule: float, process schedule of current subtask.
+        state: list, state of next step.
+
+        reward: int, reward of current action.
 
         final: bool, final signal.
-
-        info: str, detailed transaction information of simulated orders.
         '''
         if self._init == False:
             raise NotInitiateError
         if self._final == True:
             raise EnvTerminatedError
+        if action[2] > 0:
+            self._order = self._action2order(action)
+        else:
+            self._order['time'] = self._t
 
+        self._order, filled = self._engine(self._order)
+
+        if sum(filled['size']) % 100 == 0:
+            self._filled['price'] += filled['price']
+            self._filled['size']  += filled['size']
+            if self._verbose > 0:
+                self._tqdm.set_postfix(**self.metrics)
+                self._tqdm.update(sum(filled['size']))
+
+        self._t = next(self._iter)
+
+        if self._t == self._time[-1]:
+            self._final = True
+        if sum(self._filled['size']) == self._task['goal']:
+            self._final = True
+
+        # if filled rate is less than 100%, issue remain orders as market order.
+        if self._final and sum(self._filled['size']) < self._task['goal']:
+            market_order_level = 'ask1' if action[0] == 0 else 'bid1'
+            self._filled['price'].append(self._data.get_quote(self._t)[market_order_level].iloc[0])
+            self._filled['size'].append(self._task['goal'] - sum(self._filled['size']))
+            if self._verbose > 0:
+                self._tqdm.set_postfix(**self.metrics)
+                self._tqdm.update(self._task['goal'] - sum(self._filled['size']))
+
+        if self._final == True and self._verbose > 0:
+            self._tqdm.close()
+
+        reward = self.vwap - self.market_vwap
+        prices = self._data.get_quote(self._t)[self._level_space].iloc[0].values.tolist()
+        state  = [
+            self._t / 1000,
+            self._task['start'] / 1000, self._task['end'] / 1000,
+            self._task['goal'], sum(self._filled['size']),
+            *prices
+            ]
+
+        return (state, reward, self._final)
+
+
+class SemiHardConstrainTranche(BasicTranche):
+    
+    def __init__(self, tickdata, task:pd.Series, transaction_engine:callable, level_space:list, verbose=0):
+        super().__init__(tickdata, task, transaction_engine, level_space, verbose)
+
+    def reset(self):
+        super().reset()
+        state = [self._t / 1000, self._task['start'] / 1000, self._task['end'] / 1000, self._task['goal'], sum(self._filled['size'])]
+        return state
+
+    def step(self, action):
+        '''
+        Argument:
+        ---------
+        action: list, tuple, or array likes [side, level, size], where side in {0=buy, 1=sell}.
+        
+        Returns:
+        --------
+        state: list, state of next step.
+
+        reward: int, reward of current action.
+
+        final: bool, final signal.
+        '''
+        if self._init == False:
+            raise NotInitiateError
+        if self._final == True:
+            raise EnvTerminatedError
         if action[2] > 0:
             self._order = self._action2order(action)
         else:
@@ -120,45 +241,30 @@ class TrancheEnv(object):
 
         if self._t == self._time[-1]:
             self._final = True
-            if self._verbose > 0:
-                self._tqdm.close()
+        if sum(self._filled['size']) == self._task['goal']:
+            self._final = True
+            
+        if self._final == True and self._verbose > 0:
+            self._tqdm.close()
 
-        if self._final and sum(self._filled['size']) != self._task['goal']:
+        if self._final and sum(self._filled['size']) < self._task['goal']:
             reward = -999
         else:
             reward = self.vwap - self.market_vwap
 
         state = [self._t / 1000, self._task['start'] / 1000, self._task['end'] / 1000, self._task['goal'], sum(self._filled['size'])]
-        
-        return (state, reward, self._final)
 
-    def _action2order(self, action):
-        '''
-        Argument:
-        ---------
-        action: list, tuple, or array like,
-            forms as [side, level, size], where side: int, 0=buy, 1=sell.
-        Return:
-        -------
-        order: dict,
-            keys are ('side', 'price', 'size', 'pos').
-        '''
-        time  = self._t
-        side  = 'buy' if action[0] == 0 else 'sell'
-        level = self._level_space[action[1]]
-        price = self._data.get_quote(time)[level].iloc[0]
-        order = dict(time=time, side=side, price=price, size=action[2], pos=-1)
-        return order
+        return (state, reward, self._final)
 
 
 class GenerateTranches(object):
 
-    def __init__(self, goal, volume_profile, **env_kwargs):
+    def __init__(self, env, goal, volume_profile, **env_kwargs):
         ratio = [v / sum(volume_profile['volume']) for v in volume_profile['volume']]
         subgoals = [int(goal * r // 100 * 100) for r in ratio]
         subgoals[argmax(subgoals)] += goal - sum(subgoals)
         self._subtasks = pd.DataFrame(dict(start=volume_profile['start'], end=volume_profile['end'], goal=subgoals))
-        self._envs = [TrancheEnv(task=self._subtasks.loc[i], **env_kwargs) for i in self._subtasks.index]
+        self._envs = [env(task=self._subtasks.loc[i], **env_kwargs) for i in self._subtasks.index]
     
     def __iter__(self):
         for env in self._envs:
