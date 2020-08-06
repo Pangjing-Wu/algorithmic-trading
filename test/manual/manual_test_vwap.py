9b8ff5632d9025042d6206737f92ba15bd9e9829
Pangjing-Wu <pangjing.wu@outlook.com>
2020-08-06 08:32:41 -0400

    update

--- test/manual/manual_test_vwap.py
+++ test/manual/manual_test_vwap.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 from utils.dataloader import load_tickdata, load_case
 from datasource.datatype.tickdata import TickData
 from exchange.stock import GeneralExchange
-from strategies.vwap.env import GenerateTranches
+from strategies.vwap.env import GenerateTranches, HardConstrainTranche
 from strategies.vwap.agent import Baseline, Linear
 from strategies.vwap.train import BaselineTraining, EpisodicTraining
 from utils.statistic import group_trade_volume_by_time
@@ -36,22 +36,10 @@ volume_profile = group_trade_volume_by_time(trade, time, 1800000)
 
 action_encoder = lambda a: [1, 0, 0] if a == 2 else [1, a, 100]
 
-env     = GenerateTranches(200000, volume_profile, **env_params)[0]
-agent   = Baseline(side='sell', threshold=0.1)
-trainer = BaselineTraining(agent, action_map=action_encoder)
-trainer.train(env)
-guides  = trainer.action_track
+env = GenerateTranches(HardConstrainTranche, 200000, volume_profile, **env_params)[0]
 
 agent = Linear(env.observation_space_n, env.action_space_n,
                criterion = nn.MSELoss, optimizer = torch.optim.Adam)
-trainer = EpisodicTraining(agent, 10, action_map=action_encoder)
-trainer.pre_train(env, guides, 10)
-print(env.task)
-print(trainer.parameters)
+trainer = EpisodicTraining(agent, action_map=action_encoder)
 
-trainer.pre_train(env, guides, 10)
-print(env.task)
-print(trainer.parameters)
-
-r = trainer.test(env)
-print(env.task)
+trainer.train(env, 100, 'test/results/temp/temp.pth', metrics=True)
