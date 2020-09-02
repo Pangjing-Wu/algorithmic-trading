9857b34ef08e1e6f20725f876b314312e7b64bfd
Pangjing-Wu <pangjing.wu@outlook.com>
2020-09-02 04:15:39 -0400

    remove manual folder

--- test/manual_test_stastic.py
+++ test/manual_test_stastic.py
@@ -16,9 +16,3 @@ trade = data.get_trade()
 time = [34200000, 41400000, 46800000, 54000000]
 volume_per_30min = group_trade_volume_by_time(trade, time, 1800000)
 print(volume_per_30min)
-'''
-volume_total = sum(volume_per_5sec['volume'])
-ratio = [v / volume_total for v in volume_per_5sec['volume']]
-plt.plot(list(range(len(ratio))), ratio)
-plt.show()
-'''
