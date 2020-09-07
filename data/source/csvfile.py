c55924dc796d1e9c3f5e169815d98d15a88da9a6
Pangjing-Wu <pangjing.wu@outlook.com>
2020-09-07 18:47:28 +0800

    update

--- data/source/csvfile.py
+++ data/source/csvfile.py
@@ -12,18 +12,21 @@ from data.source.h2 import load_from_h2
 def h2_to_csv(stocklistdir, h2dir, savedir, user, psw, **h2_kwargs):
     with open(stocklistdir, 'r') as f:
         stocks = [s.rstrip('\n') for s in f.readlines()]
-    h2filedirs = glob.glob(os.path.join(h2dir, '*.h2.db'))
-    h2fliedirs = [h2dir.rstrip('.h2.db') for h2dir in h2filedirs]
-    for h2filedir in h2fliedirs:
-        date = os.path.basename(h2filedir)
+    h2files = glob.glob(os.path.join(h2dir, '*.h2.db'))
+    h2flies = [h2dir.rstrip('.h2.db') for h2dir in h2files]
+    for h2file in h2flies:
+        date = os.path.basename(h2file)
         for stock in stocks:
             quotedir = os.path.join(savedir, stock, 'quote')
             tradedir = os.path.join(savedir, stock, 'trade')
             os.makedirs(quotedir, exist_ok=True)
             os.makedirs(tradedir, exist_ok=True)
             try:
-                quote, trade = load_from_h2(stock, h2filedir, user, psw, **h2_kwargs)
+                quote, trade = load_from_h2(stock, h2file, user, psw, **h2_kwargs)
             except psycopg2.ProgrammingError as e:
+                print(e)
+                continue
+            except Exception as e:
                 print(e)
                 continue
             else:
@@ -32,4 +35,4 @@ def h2_to_csv(stocklistdir, h2dir, savedir, user, psw, **h2_kwargs):
 
 
 if __name__ == "__main__":
-    h2_to_csv('./stocklist', '/Volumes/data/201406', '/Volumes/data/processed', 'cra001', 'cra001')
+    h2_to_csv('./stocklist', '/Volumes/data/201408', '/Volumes/data/processed', 'cra001', 'cra001')
