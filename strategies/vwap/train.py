14256da40c67b9353bf235a2c8303166b1a8eba6
Pangjing-Wu <pangjing.wu@outlook.com>
2020-09-27 07:19:24 -0400

    add replay memory

--- strategies/vwap/train.py
+++ strategies/vwap/train.py
@@ -1,9 +1,30 @@
 import os
 import random
+from collections import namedtuple
 
 import torch
 
-random.seed(0)
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self._capacity = capacity
+        self._memory = list()
+        self._position = 0
+        self._MDP = namedtuple('MDP', ('state', 'action', 'next_state', 'reward'))
+
+    def __len__(self):
+        return len(self.memory)
+
+    def push(self, *args):
+        if len(self._memory) < self._capacity:
+            self._memory.append(None)
+        self._memory[self.position] = self._MDP(*args)
+        self._position = (self._position + 1) % self._capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
 
 class BaselineTraining(object):
 
