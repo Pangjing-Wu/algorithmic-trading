ad9b4bf0cab49f3bb51986bff8f8c4e219aaf25f
Pangjing-Wu <pangjing.wu@outlook.com>
2020-10-17 09:35:04 -0400

    update running script

--- run.sh
+++ run.sh
@@ -3,16 +3,23 @@ mode='train'
 env='hard_constrain'
 agent='linear'
 episodes=200
-device=0
-stocklist="./data/stocklist/SSE50.txt"
+device=-1
+stocklist="./data/stocklist/sample.txt"
+parallel=2
 
-cat $stocklist| while read stock
-do
-    for i in $(seq 0 $(expr $tranche - 1))
-    do
-        cmd="CUDA_VISIBLE_DEVICE=$device nohup python -u vwap.py --mode $mode --env $env --agent $agent --stock $stock
-        --side sell --episodes $episodes --level 1 --tranche_id $i 2>&1 >./logs/$mode/$stock-hard-$agent-$i-$tranche.log"
+job=0
+cat $stocklist| while read stock name; do
+    let job+=1
+    echo "strat to train $stock"
+    for i in $(seq 0 $[ $tranche - 1 ]); do
+        cmd="CUDA_VISIBLE_DEVICES=$device nohup python -u vwap.py --mode $mode --env $env --agent $agent
+        --stock $stock --side sell --episodes $episodes --level 1 --tranche_id $i --overwrite
+        2>&1 >./logs/$mode/$stock-hard-$agent-$i-$tranche.log"
         eval $cmd&
+        sleep 10
     done
-    wait
+    if [ $[$job % $parallel] == 0 ]; then
+        wait
+        echo "wait"
+    fi
 done
