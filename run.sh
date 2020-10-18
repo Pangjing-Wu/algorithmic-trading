350894647feb115da40e057f221387460db72b8e
Pangjing-Wu <pangjing.wu@outlook.com>
2020-10-18 09:38:26 -0400

    revise since pytorch support multi-threading

--- run.sh
+++ run.sh
@@ -5,12 +5,9 @@ agent='linear'
 episodes=200
 device=-1
 stocklist="./data/stocklist/sample.txt"
-parallel=2
 
-job=0
 cat $stocklist| while read stock name; do
-    let job+=1
-    echo "strat to train $stock"
+    echo "strat to train $stock at $(date)"
     for i in $(seq 0 $[ $tranche - 1 ]); do
         cmd="CUDA_VISIBLE_DEVICES=$device nohup python -u vwap.py --mode $mode --env $env --agent $agent
         --stock $stock --side sell --episodes $episodes --level 1 --tranche_id $i --overwrite
@@ -18,8 +15,6 @@ cat $stocklist| while read stock name; do
         eval $cmd&
         sleep 10
     done
-    if [ $[$job % $parallel] == 0 ]; then
-        wait
-        echo "wait"
-    fi
+    wait
+    echo "training of $stock has done at $(date)"
 done
