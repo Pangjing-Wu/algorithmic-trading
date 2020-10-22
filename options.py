b5dbe67e76498d05cf4d1e4db153d29b1ece92cc
Pangjing-Wu <pangjing.wu@outlook.com>
2020-10-22 05:22:20 -0400

    move invariant setting from arg to json config

--- options.py
+++ options.py
@@ -11,14 +11,8 @@ def parse_args(strategy):
 		parser.add_argument('--goal', default=20000, type=int, help='total sahres of trading goal per day')
 		parser.add_argument('--level', default=1, type=int, help='[1,10], available bid/ask level in action space')
 		parser.add_argument('--episodes', default=400, type=int, help='episode number of training')
-		parser.add_argument('--pre_days', default=20, type=int, help='data of pre n days for calculating volume profile')
-		parser.add_argument('--exchange' , default='general', type=str, help='specify exchange')
-		parser.add_argument('--time_range', default=[34200000, 41400000, 46800000, 53700000], type=list, nargs='+', help='legal time range of transaction')
-		parser.add_argument('--interval'  , default=1800000, type=int, help='time length of each tranche')
 		parser.add_argument('--tranche_id', default=0, type=int, help='specify tranche of each day')
 		parser.add_argument('--overwrite', action='store_true', help='train a new model and overwrite existed')
-		parser.add_argument('--hist_quote', default=5, type=int, help='specify number of historical quote in state')
-		parser.add_argument('--wait_t', default=3, type=int, help='wait time before traing')
 	else:
 		raise ValueError('Unknown strategy.')
 
