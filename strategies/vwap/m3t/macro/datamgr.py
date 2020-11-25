5ddfe8a365f8cec5d73f205479507ae25184d45a
Pangjing-Wu <pangjing.wu@outlook.com>
2020-11-25 08:44:03 -0500

    add new property

--- strategies/vwap/m3t/macro/datamgr.py
+++ strategies/vwap/m3t/macro/datamgr.py
@@ -58,33 +58,54 @@ class VolumeProfileDataset(object):
         return 1
 
     @property
+    def n_tranche(self):
+        return self.__n_tranche
+
+    @property
     def train_set(self):
         return self.__train_set
+
+    @property
+    def train_date(self):
+        return self.__train_date
 
     @property
     def valid_set(self):
         return self.__valid_set
 
     @property
+    def valid_date(self):
+        return self.__valid_date
+
+    @property
     def test_set(self):
         return self.__test_set
 
+    @property
+    def test_date(self):
+        return self.__test_date
+
     def __build_dataset(self):
+        date = list()
         dataset = list()
         for data in self.__dataset:
-            profile = volume_profile(
-                data.trade,
-                self.__time_range,
-                self.__interval
-                )
+            date.append(data.date)
+            profile = volume_profile(data.trade, self.__time_range, self.__interval)
             dataset.append(profile['volume'].values)
+        self.__n_tranche = profile.shape[0]
         X, y = self.__rolling_generate(np.array(dataset), self.__hist_len)
         X, y = torch.from_numpy(X), torch.from_numpy(y)
-        n_train = int(X.shape[0] * self.__split[0])
-        n_valid = int(X.shape[0] * self.__split[1]) + n_train
+        n_train = int(X.shape[0] * self.__split[0] // self.__n_tranche * self.__n_tranche)
+        n_valid = int(X.shape[0] * self.__split[1] // self.__n_tranche * self.__n_tranche + n_train)
         self.__train_set = SupervisedData(X[:n_train], y[:n_train])
         self.__valid_set = SupervisedData(X[n_train:n_valid], y[n_train:n_valid])
         self.__test_set  = SupervisedData(X[n_valid:], y[n_valid:])
+        date = date[self.__hist_len:]
+        n_train = int(n_train // self.__n_tranche)
+        n_valid = int(n_valid // self.__n_tranche)
+        self.__train_date = date[:n_train]
+        self.__valid_date = date[n_train:n_valid]
+        self.__test_date  = date[n_valid:]
 
     def __check_split(self, split:List[float]):
         if len(split) != 3:
