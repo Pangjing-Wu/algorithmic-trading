7fffe2e0d8afb6adc701d5bf58a25db9c4fb94cc
Pangjing-Wu <pangjing.wu@outlook.com>
2020-11-25 03:30:57 -0500

    format echo

--- scripts/m3t/train-macro.sh
+++ scripts/m3t/train-macro.sh
@@ -3,14 +3,14 @@ epoch=200
 models=("Linear" "MLP" "LSTM")
 
 cat $stocklist | while read stock name; do
-    echo " $name($stock) start at $(date)"
+    echo "$name($stock) start at $(date)"
     for model in ${models[@]}; do
-        echo "start to train $model at $(date)"
+        echo "     [START] train $model at $(date)"
         cmd="python -u ./scripts/m3t/macro/train.py 
             --stock $stock --epoch 200 --model $model --checkpoint 0 --cuda
             2>&1 >./results/logs/vwap/m3t/macro/$stock-$model.log"
         eval $cmd
-        echo "training done at $(date)"
+        echo "     [DONE] $(date)"
     done
     echo "$name($stock) has done at $(date)"
 done
