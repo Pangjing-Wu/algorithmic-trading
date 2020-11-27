19c43f51dd46004e88188ad633315c857ddbe1d4
Pangjing-Wu <pangjing.wu@outlook.com>
2020-11-27 11:19:22 -0500

    improve TrancheDataset

--- strategies/vwap/m3t/micro/datamgr.py
+++ strategies/vwap/m3t/micro/datamgr.py
@@ -4,6 +4,8 @@ from typing import List, Tuple
 import torch
 import numpy as np
 
+from ..macro.profile import get_tranche_time
+
 
 class TrancheDataset(object):
     ''' get tranche dataset
@@ -13,14 +15,24 @@ class TrancheDataset(object):
         dataset: tickdata dataset, e.g.: {CSVDataset/H2Dataset}.
     '''
 
-    def __init__(self, dataset, split:List[float], 
-                 time:Tuple[int], drop_length=0):
+    def __init__(self, dataset, split:List[float], i_tranche:int,
+                 time_range:List[int], interval=int, drop_length=0):
         self.__check_split(split)
-        self.__time    = time
+        self.__times   = get_tranche_time(time_range, interval)
+        self.__n       = len(self.__times)
+        self.__time    = self.__times[i_tranche]
         self.__split   = split
         self.__dates   = dataset.dates[drop_length:]
         self.__dataset = dataset[drop_length:]
         self.__build_dataset()
+
+    @property
+    def n(self):
+        return self.__n
+
+    @property
+    def time(self):
+        return self.__time
 
     @property
     def train_set(self)->list:
@@ -37,6 +49,9 @@ class TrancheDataset(object):
     @property
     def test_date(self)->list:
         return self.__test_date
+
+    def set_tranche(self, i):
+        self.__time = self.__times[i]
 
     def __build_dataset(self):
         n = len(self.__dataset)
