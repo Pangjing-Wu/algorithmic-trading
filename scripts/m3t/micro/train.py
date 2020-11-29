308f741213aa86fedbc9e246d99fa804afda6837
Pangjing-Wu <pangjing.wu@outlook.com>
2020-11-29 09:17:01 -0500

    fix a bug in discriminating model

--- scripts/m3t/micro/train.py
+++ scripts/m3t/micro/train.py
@@ -25,11 +25,11 @@ def parse_args():
     parser.add_argument('--stock', type=str, help='stock code')
     parser.add_argument('--model', type=str, help='RL agent based model {Linear/HybridLSTM}')
     parser.add_argument('--agent', type=str, help='RL agent {QLearning/}')
-    parser.add_argument('--episode', default=200, type=int, help='episode for training')
+    parser.add_argument('--episode', type=int, help='episode for training')
+    parser.add_argument('--i_tranche', type=int, help='train model for i tranche, start from 1')
     parser.add_argument('--checkpoint', default=0, type=int, help='save model per checkpoint')
     parser.add_argument('--quote_length', default=1, type=int, help='length of quote data for training')
     parser.add_argument('--start_episode', default=0, type=int, help='start episode')
-    
     return parser.parse_args()
 
 
@@ -44,7 +44,7 @@ def generate_tranche_envs(dataset, env, time, args, config):
                 tickdata=data,
                 task=task,
                 exchange=exchange,
-                level=1,
+                level=config['m3t']['micro']['level'],
                 side=config['m3t']['side'],
                 quote_length=args.quote_length
                 )
@@ -59,66 +59,65 @@ def main(args, config):
     else:
         device = torch.device('cpu')
 
-    times = get_tranche_time(
-        time_range=config['data']['times'], 
-        interval=config['m3t']['interval']
-        )
-    n_tranches = len(times)
     dataset = CSVDataset(config['data']['path'], args.stock)
 
-    for n, time in enumerate(times):
-        tranches = TrancheDataset(
-            dataset=dataset,
-            split=config['m3t']['split'],
-            time=time,
-            history_length=config['m3t']['macro']['n_history']
+    tranches = TrancheDataset(
+        dataset=dataset,
+        split=config['m3t']['micro']['split'],
+        i_tranche=args.i_tranche,
+        time_range=config['data']['times'],
+        interval=config['m3t']['interval'],
+        drop_length=config['m3t']['macro']['n_history']
+        )
+
+    if args.env == 'Historical':
+        env = HistoricalTranche
+    elif args.env == 'Recurrent':
+        env = RecurrentTranche
+    else:
+        raise ValueError('unknown environment')
+
+    envs = generate_tranche_envs(tranches.train_set, env,
+                                 tranches.time, args, config)
+
+    model_config = config['m3t']['micro']['model']
+    if args.model == 'Linear':
+        model = Linear(
+            input_size=envs[0].observation_space_n, 
+            output_size=envs[0].action_space_n,
+            device=device
             )
-        if args.env == 'Historical':
-            env = HistoricalTranche
-        elif args.env == 'Recurrent':
-            env = RecurrentTranche
-        else:
-            raise ValueError('unknown environment')
-        train_envs = generate_tranche_envs(tranches.train_set, env, time, args, config)
-        valid_envs = generate_tranche_envs(tranches.valid_set, env, time, args, config)
-
-        model_config = config['m3t']['micro']['model']
-        if args.model == 'Linear':
-            model = Linear(
-                input_size=train_envs[0].observation_space_n, 
-                output_size=train_envs[0].action_space_n,
-                device=device
-                )
-        elif args.model == 'lstm':
-            model = HybridLSTM(
-                input_size=train_envs[0].observation_space_n,
-                output_size=train_envs[0].action_space_n,
-                hidden_size=model_config[args.model]['hidden_size'],
-                num_layers=model_config[args.model]['num_layers'],
-                dropout=model_config[args.model]['dropout'],
-                device=device
-                )
-
-        if args.agent == 'QLearning':
-            agent = QLearning(device=device, **config['m3t']['micro']['agent']['QLearning'])
-        else:
-            raise ValueError('unkonwn agent')
-
-        model_dir = os.path.join(config['model_dir'], 'm3t', 'micro', args.stock,
-                                 args.agent, "%s-%d" % (args.model, args.quote_length),
-                                 '%d-%d' % (n+1, n_tranches))
-        criterion = nn.MSELoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=model_config['lr'])
-        agent.train(
-            train_envs=train_envs,
-            val_envs=valid_envs,
-            model=model,
-            model_dir=model_dir,
-            criterion=criterion,
-            optimizer=optimizer,
-            episode=args.episode,
-            start_episode=args.start_episode
+    elif args.model == 'Lstm':
+        model = HybridLSTM(
+            input_size=envs[0].observation_space_n,
+            output_size=envs[0].action_space_n,
+            hidden_size=model_config[args.model]['hidden_size'],
+            num_layers=model_config[args.model]['num_layers'],
+            dropout=model_config[args.model]['dropout'],
+            device=device
             )
+    else:
+        raise ValueError('unknown model.')
+
+    if args.agent == 'QLearning':
+        agent = QLearning(device=device, **config['m3t']['micro']['agent']['QLearning'])
+    else:
+        raise ValueError('unkonwn agent')
+
+    model_dir = os.path.join(config['model_dir'], 'm3t', 'micro', args.stock,
+                                args.agent, "%s-%d" % (args.model, args.quote_length),
+                                '%d-%d' % (args.i_tranche, tranches.n))
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=model_config['lr'])
+    agent.train(
+        envs=envs,
+        model=model,
+        model_dir=model_dir,
+        criterion=criterion,
+        optimizer=optimizer,
+        episode=args.episode,
+        start_episode=args.start_episode
+        )
         
 
 if __name__ == '__main__':
