ddec6914ee59f02a24b2d2c012ffef2089eeb069
Pangjing-Wu <pangjing.wu@outlook.com>
2020-12-03 11:17:15 -0500

    update

--- scripts/m3t/train-micro.sh
+++ scripts/m3t/train-micro.sh
@@ -1,18 +1,22 @@
-tranche=8
-mode='train'
-env='recurrent_hard_constrain'
-agent='lstm'
-episodes=200
+tranches=8
+model='Linear'
+agent='QLearning'
+env='Historical'
+reward='sparse'
+eps=0.5
+streps='05'
+episode=10000
 device=0
+quote_length=1
 stocklist="./data/stocklist/sample.txt"
 
 cat $stocklist| while read stock name; do
-    echo "strat to train $stock at $(date)"
-    for i in $(seq 0 $[ $tranche - 1 ]); do
-        $device = $[$i%2]
-        cmd="CUDA_VISIBLE_DEVICES=$device nohup python -u vwap.py --mode $mode --env $env --agent $agent
-        --stock $stock --side sell --episodes $episodes --level 1 --tranche_id $i --overwrite
-        2>&1 >./logs/$mode/$stock-hard-$agent-$i-$tranche.log"
+    for i in $(seq 1 $tranches); do
+    echo "strat to train $stock tranche $i/$tranches at $(date)"
+        cmd="CUDA_VISIBLE_DEVICES=$device python -u ./scripts/m3t/micro/train.py 
+            --stock $stock --episode $episode --agent $agent --eps $eps --i_tranche $i
+            --model $model --env $env --reward $reward --quote_length $quote_length --cuda
+            2>&1 >./results/logs/vwap/m3t/micro/$stock-$model-eps$streps-$reward-len$quote_length-$i-$tranches.log"
         eval $cmd&
         sleep 10
     done
