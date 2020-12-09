19cf7cf5e3b496567ef0a233bc782f874ec2207b
Pangjing-Wu <pangjing.wu@outlook.com>
2020-12-09 06:50:02 -0500

    update

--- scripts/hrl/train.sh
+++ scripts/hrl/train.sh
@@ -1,21 +1,24 @@
-tranches=8
-model='HybridLSTM'
+models=('HybridLSTM' 'HybridAttenBiLSTM')
 agent='HierarchicalQ'
-env='Recurrent'
-reward='dense'
+rewards=('dense' 'sparse')
 eps=1.0
 streps='10'
 episode=10000
-device=1
 quote_length=5
 stocklist="./data/stocklist/sample-8.txt"
 
 cat $stocklist| while read stock name; do
     echo "strat to train $stock at $(date)"
-    cmd="CUDA_VISIBLE_DEVICES=$device python -u ./scripts/hrl/train.py 
-        --stock $stock --episode $episode --agent $agent --eps $eps --model $model 
-        --env $env --reward $reward --quote_length $quote_length --cuda
-        2>&1 >./results/logs/vwap/hrl/$stock-eps$streps-$reward-len$quote_length.log"
-    eval $cmd&
+    for model in ${models[@]}; do
+        if [ $model == 'HybridLSTM' ]; then device=0; else device=1; fi
+        for reward in ${rewards[@]}; do
+            cmd="CUDA_VISIBLE_DEVICES=$device python -u ./scripts/hrl/train.py 
+                --stock $stock --episode $episode --agent $agent --eps $eps
+                --model $model --reward $reward --quote_length $quote_length --cuda
+                2>&1 >./results/logs/vwap/hrl/$stock-$model-eps$streps-$reward-len$quote_length.log"
+            echo $cmd&
+            sleep 2
+        done
+    done
 done
-echo "training has done at $(date)"
+echo "training script has done at $(date)"
