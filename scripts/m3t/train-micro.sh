f53aef18aac4059815bb29beffcc5a0994155c34
Pangjing-Wu <pangjing.wu@outlook.com>
2020-12-16 06:50:19 -0500

    update

--- scripts/m3t/train-micro.sh
+++ scripts/m3t/train-micro.sh
@@ -1,25 +1,27 @@
 tranches=8
-model='Linear'
+models=('Linear' 'HybridLSTM')
 agent='QLearning'
-env='Historical'
-reward='sparse'
-eps=0.5
-streps='05'
+rewards=('sparse' 'dense')
+eps=1.0
+streps='10'
 episode=10000
-device=0
-quote_length=1
-stocklist="./data/stocklist/sample.txt"
+quote_length=5
+stocklist="./data/stocklist/sample-8.txt"
 
 cat $stocklist| while read stock name; do
     for i in $(seq 1 $tranches); do
-    echo "strat to train $stock tranche $i/$tranches at $(date)"
-        cmd="CUDA_VISIBLE_DEVICES=$device python -u ./scripts/m3t/micro/train.py 
-            --stock $stock --episode $episode --agent $agent --eps $eps --i_tranche $i
-            --model $model --env $env --reward $reward --quote_length $quote_length --cuda
-            2>&1 >./results/logs/vwap/m3t/micro/$stock-$model-eps$streps-$reward-len$quote_length-$i-$tranches.log"
-        eval $cmd&
-        sleep 10
+        echo "strat to train $stock tranche $i/$tranches at $(date)"
+        for model in ${models[@]}; do
+            if [ $model == 'Linear' ]; then device=0; else device=1; fi
+            for reward in ${rewards[@]}; do
+                cmd="CUDA_VISIBLE_DEVICES=$device python -u ./scripts/m3t/micro/train.py 
+                    --stock $stock --episode $episode --agent $agent --eps $eps --i_tranche $i
+                    --model $model --reward $reward --quote_length $quote_length --cuda
+                    2>&1 >./results/logs/vwap/m3t/micro/$stock-$model-eps$streps-$reward-len$quote_length-$i-$tranches.log"
+                eval $cmd&
+                sleep 2
+            done
+        done
     done
     wait
-    echo "training of $stock has done at $(date)"
 done
