b8a0940219646e506829f88bd0686f98dc9ec346
Pangjing-Wu <pangjing.wu@outlook.com>
2021-01-10 23:01:11 -0500

    init commit m3t test

---
+++ scripts/m3t/test.py
@@ -0,0 +1,211 @@
+import argparse
+import json
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+
+sys.path.append('./')
+from data.tickdata import CSVDataset
+from exchange.stock import AShareExchange
+from strategies.vwap.m3t.datamgr import VolumeProfileDataset, TrancheDataset 
+from strategies.vwap.m3t.micro.env import (BaselineTranche, HistoricalTranche,
+                                           RecurrentTranche)
+from strategies.vwap.m3t.micro.trader import MicroTrader
+from strategies.vwap.m3t.model import (LSTM, HybridLSTM, Linear, MacroBaseline,
+                                       MicroBaseline)
+
+dotmut = lambda x, y: sum([a * b for a, b in zip(x, y)])
+
+def parse_args():
+    parser = argparse.ArgumentParser('test macro trader')
+    parser.add_argument('--goal', type=int, help='total order volume')
+    parser.add_argument('--stock', type=str, help='stock code')
+    parser.add_argument('--macro', type=str, help='macro model {Baseline/LSTM}')
+    parser.add_argument('--micro', type=str, help='RL agent based model {Baseline/Linear/HybridLSTM}')
+    parser.add_argument('--agent', type=str, help='RL agent {QLearning/}')
+    parser.add_argument('--reward', type=str, help='environment reward type {sparse/dense}')
+    parser.add_argument('--macro_epoch', type=int, help='macro model epoch')
+    parser.add_argument('--micro_episode', type=int, help='micro model episode')
+    parser.add_argument('--quote_length', type=int, help='length of quote data for training')
+    return parser.parse_args()
+
+
+def generate_tranche_envs(dataset, env, volume_profiles, time, args, config)->list:
+    envs = list()
+    for data, vp in zip(dataset, volume_profiles):
+        subgoal = int(args.goal * vp // 100 * 100)
+        trade = data.trade.between(time[0],time[1])
+        # remove environment with bad liquidity
+        if trade['size'].sum() < subgoal * 100:
+            envs.append(None)
+        else:
+            task = pd.Series(dict(start=time[0], end=time[1], goal=subgoal))
+            exchange = AShareExchange(data, wait_trade=config['exchange']['wait_trade'])
+            envs.append(
+                env(
+                    tickdata=data,
+                    task=task,
+                    exchange=exchange,
+                    level=config['m3t']['micro']['level'],
+                    side=config['m3t']['side'],
+                    quote_length=args.quote_length,
+                    reward=args.reward
+                    )
+                )
+    return envs
+
+
+def main(args, config):
+    device = torch.device('cpu')
+    dataset = CSVDataset(config['data']['path'], args.stock)
+
+    # macro dataset
+    vp_data = VolumeProfileDataset(
+        dataset=dataset,
+        split=config['m3t']['micro']['split'], 
+        time_range=config['data']['times'],
+        interval=config['m3t']['interval'],
+        history_length=config['m3t']['macro']['n_history']
+        )
+
+    macro_config = config['m3t']['macro']['model']
+    micro_config = config['m3t']['micro']['model']
+
+    # macro model
+    if args.macro == 'Baseline':
+        macro = MacroBaseline()
+    elif args.macro == 'LSTM':
+        macro = LSTM(
+            input_size=1,
+            output_size=1,
+            hidden_size=macro_config[args.macro]['hidden_size'],
+            num_layers=macro_config[args.macro]['num_layers'],
+            dropout=macro_config[args.macro]['dropout']
+            )
+    else:
+        raise ValueError('unknown macro model')
+
+    # load macro model
+    epoch = args.macro_epoch if args.macro_epoch != -1 else 'best'
+    macro_file = os.path.join(config['model_dir'], 'm3t', 'macro',
+                                args.stock, args.macro, '%s.pt' % epoch)
+    macro_file = None if isinstance(macro, MacroBaseline) else macro_file
+    if macro_file is not None:
+        macro.load_state_dict(torch.load(macro_file))
+    macro.eval()
+
+    # micro environment
+    if args.micro == 'Linear':
+        env = HistoricalTranche
+    elif args.micro == 'HybridLSTM':
+        env = RecurrentTranche
+    elif args.micro == 'Baseline':
+        env = BaselineTranche
+    else:
+        raise ValueError('unknown environment')
+
+    # generate environments
+    train_envs = list()
+    test_envs  = list()
+    for i in range(vp_data.n_tranche):
+        train_vp = macro(vp_data.train_set[i].X).reshape(-1).numpy()
+        test_vp  = macro(vp_data.test_set[i].X).reshape(-1).numpy()
+        # micro dataset
+        tranches = TrancheDataset(
+            dataset=dataset,
+            split=config['m3t']['micro']['split'],
+            i_tranche=i+1,
+            time_range=config['data']['times'],
+            interval=config['m3t']['interval'],
+            drop_length=config['m3t']['macro']['n_history']
+            )
+        train_env = generate_tranche_envs(tranches.train_set, env, train_vp,
+                                          tranches.time, args, config)
+        test_env  = generate_tranche_envs(tranches.test_set, env, test_vp,
+                                          tranches.time, args, config)
+        train_envs += train_env
+        test_envs  += test_env
+    train_envs = np.array(train_envs, dtype=object).reshape(8, -1).T
+    test_envs  = np.array(test_envs, dtype=object).reshape(8, -1).T
+
+    # load agents
+    agents = list()
+    for i in range(tranches.n):
+        if args.micro == 'Linear':
+            micro = Linear(
+                input_size=train_envs[0,0].observation_space_n, 
+                output_size=train_envs[0,0].action_space_n,
+                device=device
+                )
+        elif args.micro == 'HybridLSTM':
+            micro = HybridLSTM(
+                input_size=train_envs[0,0].observation_space_n,
+                output_size=train_envs[0,0].action_space_n,
+                hidden_size=micro_config[args.micro]['hidden_size'],
+                num_layers=micro_config[args.micro]['num_layers'],
+                dropout=micro_config[args.micro]['dropout'],
+                device=device
+                )
+        elif args.micro == 'Baseline':
+            micro = MicroBaseline(
+                side=config['m3t']['side'],
+                level=config['m3t']['micro']['level'],
+                lb=1.01,
+                ub=1.1
+                )
+        else:
+            raise ValueError('unknown model.')
+
+        micro_file = os.path.join(config['model_dir'], 'm3t', 'micro',
+                                 args.stock, args.agent,
+                                 '%s-len%d' % (args.micro, args.quote_length),
+                                 '%s-eps10' % args.reward,
+                                 '%d-%d' % (i+1, tranches.n),
+                                 '%d.pt' % args.micro_episode)
+        if not isinstance(micro, MicroBaseline):
+            weight = torch.load(micro_file, map_location='cpu')
+            micro.load_state_dict(weight)
+        micro.eval()
+        agents.append(MicroTrader(micro))
+
+    #print args
+    print('goal: %d,' % args.goal, end='')
+    print('macro: %s, epoch: %d, ' % (args.macro, args.macro_epoch), end='')
+    print('agent: %s, micro: %s, ' % (args.agent, args.micro), end='')
+    print('micro episode: %d, ' % args.micro_episode, end='')
+    print('eps:1.0, reward type: %s, ' % args.reward, end='')
+    print('quote length: %d, ' % args.quote_length)
+
+    # test on train set
+    for envs in [train_envs, test_envs]:
+        for t in range(envs.shape[0]):
+            trade = dataset[envs[t,0]._data.date].trade
+            market_vwap = dotmut(trade['price'], trade['size']) / trade['size'].sum()
+            filled = dict(price=[], size=[])
+            slippages = []
+            for i in range(envs.shape[1]):
+                env = envs[t,i]
+                if env is None:
+                    continue
+                else:
+                    agents[i](env)
+                    filled['price'] += env.filled['price']
+                    filled['size']  += env.filled['size']
+            vwap = dotmut(filled['price'], filled['size']) / sum(filled['size'])
+            slippages.append((vwap - market_vwap) * 10000)
+        if envs is train_envs:
+            print('test results of training set')
+        else:
+            print('test results of test set')
+        print(pd.Series(slippages).describe())
+                    
+if __name__ == '__main__':
+    args  = parse_args()
+    config = json.load(open('./config/vwap.json', 'r'), encoding='utf-8')
+    main(args, config)
+
+# python ./scripts/m3t/test.py --goal 200000 --stock 600000 --macro Baseline --micro Baseline --agent QLearning --reward dense --micro_episode 10000 --quote_length 5
---
+++ strategies/vwap/m3t/datamgr.py
@@ -0,0 +1,189 @@
+import sys
+from typing import List
+
+import torch
+import numpy as np
+
+from .macro.profile import volume_profile, get_tranche_time
+
+
+class SupervisedData(object):
+
+    def __init__(self, X, y):
+        self.__X = X if torch.is_tensor(X) else torch.tensor(X)
+        self.__y = y if torch.is_tensor(y) else torch.tensor(y)
+        self.__X = self.__X.float()
+        self.__y = self.__y.float().reshape(-1, 1)
+
+    def __len__(self):
+        return len(self.__X)
+
+    def __str__(self):
+        return "%s" % dict(x=self.__X, y=self.__y)
+
+    @property
+    def X(self):
+        return self.__X
+    
+    @property
+    def y(self):
+        return self.__y
+
+
+class VolumeProfileDataset(object):
+    ''' get volume profile dataset
+
+    Arguments:
+    ----------
+        dataset: tickdata dataset, e.g.: {CSVDataset/H2Dataset}.
+    '''
+
+    def __init__(self, dataset, split:List[float], 
+                 time_range:List[int], interval:int,
+                 history_length=20):
+        self.__check_split(split)
+        self.__dataset    = dataset
+        self.__split      = split
+        self.__time_range = time_range
+        self.__interval   = interval
+        self.__hist_len   = history_length
+        self.__build_dataset()
+
+    @property
+    def X_len(self):
+        return self.__hist_len
+
+    @property
+    def y_len(self):
+        return 1
+
+    @property
+    def n_tranche(self):
+        return self.__n_tranche
+
+    @property
+    def train_set(self):
+        '''shape: [i_tranche].X[n_sample, n_features]
+                  [i_tranche].y[n_sample, 1]
+        '''
+        return self.__train_set
+
+    @property
+    def train_date(self):
+        return self.__train_date
+
+    @property
+    def test_set(self):
+        '''shape: [i_tranche].X[n_sample, n_features]
+                  [i_tranche].y[n_sample, 1]
+        '''
+        return self.__test_set
+
+    @property
+    def test_date(self):
+        return self.__test_date
+
+    def __build_dataset(self):
+        date = list()
+        dataset = list()
+        for data in self.__dataset:
+            date.append(data.date)
+            profile = volume_profile(data.trade, self.__time_range, self.__interval)
+            dataset.append(profile['volume'].values)
+        self.__n_tranche = profile.shape[0]
+        X, y = self.__rolling_generate(np.array(dataset), self.__hist_len)
+        X = torch.from_numpy(X).reshape(-1, self.__n_tranche, self.__hist_len)
+        y = torch.from_numpy(y).reshape(-1, self.__n_tranche)
+        n_train = int(X.shape[0] * self.__split[0])
+        self.__train_set = [SupervisedData(X[:n_train, i], y[:n_train, i]) for i in range(self.__n_tranche)]
+        self.__test_set  = [SupervisedData(X[n_train:, i], y[n_train:, i]) for i in range(self.__n_tranche)]
+        date = date[self.__hist_len:]
+        self.__train_date = date[:n_train]
+        self.__test_date  = date[n_train:]
+
+    def __check_split(self, split:List[float]):
+        if len(split) != 2:
+            raise ValueError('the length of split must be 2.')
+        if sum(split) != 1:
+            raise ValueError('the sum of split must be 1.')
+
+    def __rolling_generate(self, array:np.array, n=int):
+        X, y = list(), list()
+        for i in range(array.shape[1]):
+            for j in range(n, array.shape[0]):
+                X.append(array[j-n:j, i])
+                y.append(array[j, i])
+        return np.array(X), np.array(y)
+
+    
+class TrancheDataset(object):
+    ''' get tranche dataset
+
+    Arguments:
+    ----------
+        dataset: tickdata dataset, e.g.: {CSVDataset/H2Dataset}.
+        i_tranche: int, tranche id, start from 1.
+    '''
+
+    def __init__(self, dataset, split:List[float], i_tranche:int,
+                 time_range:List[int], interval:int, drop_length=0):
+        self.__check_split(split)
+        self.__times   = get_tranche_time(time_range, interval)
+        self.__n       = len(self.__times)
+        self.__split   = split
+        self.__dates   = dataset.dates[drop_length:]
+        self.__dataset = dataset[drop_length:]
+        self.set_tranche(i_tranche)
+
+    @property
+    def n(self):
+        return self.__n
+
+    @property
+    def time(self):
+        return self.__time
+
+    @property
+    def train_set(self)->list:
+        return self.__train_set
+
+    @property
+    def train_date(self)->list:
+        return self.__train_date
+
+    @property
+    def test_set(self)->list:
+        return self.__test_set
+
+    @property
+    def test_date(self)->list:
+        return self.__test_date
+
+    def set_tranche(self, i:int):
+        '''
+        Arguments:
+        ----------
+        i_tranche: int, tranche id, start from 1.
+        '''
+        self.__check_tranche(i)
+        self.__time = self.__times[i-1]
+        self.__build_dataset()
+        return self
+
+    def __build_dataset(self):
+        n = len(self.__dataset)
+        n_train = int(n * self.__split[0])
+        self.__train_set  = [data.between(*self.__time) for data in self.__dataset[:n_train]]
+        self.__test_set   = [data.between(*self.__time) for data in self.__dataset[n_train:]]
+        self.__train_date = self.__dates[:n_train]
+        self.__test_date  = self.__dates[n_train:]
+
+    def __check_split(self, split:List[float]):
+        if len(split) != 2:
+            raise ValueError('the length of split must be 3.')
+        if sum(split) != 1:
+            raise ValueError('the sum of split must be 1.')
+
+    def __check_tranche(self, i):
+        if i < 1 or i > len(self.__times):
+            raise ValueError('tranche id not in range.')
